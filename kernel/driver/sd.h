31f09633d88a779e38fee3c73838d6ed00690591
chenzhuo <chenzhuo757@zju.edu.cn>
2019-10-29 23:26:26 +0800

    update

--- kernel/driver/sd.h
+++ kernel/driver/sd.h
@@ -1,9 +1,9 @@
-#ifndef _SD_H
-#define _SD_H
+#ifndef SD_H
+#define SD_H
 
 #include <driver/sd.h>
 
 int sd_write_sector_blocking(int id, void* buffer);
 int sd_read_sector_blocking(int id, void* buffer);
 
-#endif
+#endif // SD_H
--- kernel/driver/vga.h
+++ kernel/driver/vga.h
@@ -1,6 +1,6 @@
-#ifndef _VGA_H
-#define _VGA_H
+#ifndef VGA_H
+#define VGA_H
 
 #include <driver/vga.h>
 
-#endif
+#endif // VGA_H
