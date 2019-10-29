aaea7bb240d43acc70b2d29086c5bbde5cccbfe5
SolarBear251 <948697775@qq.com>
2019-10-29 18:44:56 +0800

    添加了一些类型定义，如uint,uchar等

--- include/os/type.h
+++ include/os/type.h
@@ -1,8 +1,17 @@
 #ifndef OS_TYPE_H
 #define OS_TYPE_H
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned long u32;
+typedef unsigned char   u8;
+typedef unsigned short  u16;
+typedef unsigned long   u32;
+
+typedef unsigned int    uint;
+typedef unsigned char   uchar;
+typedef unsigned short  ushort;
+typedef unsigned long   ulong;
+
+typedef unsigned char   Byte;
+typedef unsigned short  Word;
+typedef unsigned long   DWord;
 
 #endif // OS_TYPE_H
