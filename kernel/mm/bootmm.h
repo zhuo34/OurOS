9a26455df9573d9f94f510a202f68303cab799cd
Zhuo Chen <chenzhuo757@zju.edu.cn>
2019-11-03 12:32:04 +0800

    modify bootmm

--- kernel/mm/bootmm.h
+++ kernel/mm/bootmm.h
@@ -3,11 +3,11 @@
 
 #include <ouros/bootmm.h>
 
-uint insert_bootmm_info(bootmm_struct *mm, uint addr_start, uint length, uint type);
-uint set_bootmm_info(bootmm_struct *mm, uint index, uint addr_start, uint length, uint type);
-uint delete_bootmm_info(bootmm_struct *mm, uint index);
-uint split_bootmm_info(bootmm_struct *mm, uint index, uint split_addr);
+uint insert_bootmm_info(bootmm_sys *mm, uint addr_start, uint length, uint type);
+uint set_bootmm_info(bootmm_sys *mm, uint index, uint addr_start, uint length, uint type);
+uint delete_bootmm_info(bootmm_sys *mm, uint index);
+uint split_bootmm_info(bootmm_sys *mm, uint index, uint split_addr);
 
-uint find_pages(bootmm_struct *mm, uint page_cnt, uint pfn_start, uint pfn_end, uint pfn_align);
+uint find_pages(bootmm_sys *mm, uint page_cnt, uint pfn_start, uint pfn_end, uint pfn_align);
 
 #endif // BOOTMM_H
