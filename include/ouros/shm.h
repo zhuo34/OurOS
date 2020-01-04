199d454af010b800c3d7fcd8f3707a114e2fff57
Zhuo Chen <chenzhuo757@zju.edu.cn>
2020-01-05 05:21:15 +0800

    update shm

--- include/ouros/shm.h
+++ include/ouros/shm.h
@@ -1,22 +1,21 @@
 #ifndef OUROS_SHM_H
 #define OUROS_SHM_H
 
-#include <linux/list.h>
+#define MAX_SHM_NUM 8
 
 struct shm_struct {
 	uint pfn_start;
 	uint pfn_num;
-	struct list_head list;
 };
 
-struct shm_pool_struct {
-	uint alloc_num;
-	struct list_head alloc_list;
-};
+// struct shm_pool_struct {
+// 	// uint alloc_num;
+// 	struct shm_struct *alloc_list[MAX_SHM_NUM];
+// };
 
 void init_shm_pool();
 
-struct shm_struct *shmget(uint size);
-void shmat(struct shm_struct *shm, void *vaddr);
+int shmget(uint size);
+void shmat(int shmid, void *vaddr);
 
 #endif // OUROS_SHM_H
--- kernel/vm/shm.c
+++ kernel/vm/shm.c
@@ -4,34 +4,43 @@
 #include <ouros/buddy.h>
 #include <ouros/vm.h>
 
-struct shm_pool_struct shm_pool;
+struct shm_struct *shm_pool[MAX_SHM_NUM];
 
 void init_shm_pool()
 {
-	shm_pool.alloc_num = 0;
-	INIT_LIST_HEAD(&shm_pool.alloc_list);
+	kernel_memset(shm_pool, 0, sizeof(struct shm_struct *) * MAX_SHM_NUM);
 }
 
-struct shm_struct *shmget(uint size)
+int shmget(uint size)
 {
+	int ret = -1;
 	if (!size)
-		return nullptr;
+		return ret;
+	for (int i = 0; i < MAX_SHM_NUM; i++) {
+		if (!shm_pool[i]) {
+			ret = i;
+			break;
+		}
+	}
+	if (ret == -1) return ret;
 	void *paddr = alloc_pages(size);
 	if (!paddr) {
-		return nullptr;
+		return ret;
 	}
 	struct shm_struct *shm = (struct shm_struct *)kmalloc(sizeof(struct shm_struct));
 	shm->pfn_start = get_pgn(get_page_by_paddr(paddr));
 	shm->pfn_start = upper_align(size, 1 << PAGE_SHIFT) >> PAGE_SHIFT;
-	list_add_tail(&shm->list, &shm_pool.alloc_list);
-	shm_pool.alloc_num ++;
+	shm_pool[ret] = shm;
+	return ret;
 }
 
-void shmat(struct shm_struct *shm, void *vaddr)
+void shmat(int shmid, void *vaddr)
 {
+	if (shmid < 0 || shmid >= MAX_SHM_NUM || !shm_pool[shmid])
+		return;
 	void *p = (void *)lower_align((uint)vaddr, 1 << PAGE_SHIFT);
-	for (int i = 0; i < shm->pfn_num; i++) {
-		get_pte(mm_current->pgd, p)->tlb_entry.reg.PFN = shm->pfn_start + i;
+	for (int i = 0; i < shm_pool[shmid]->pfn_num; i++) {
+		get_pte(mm_current->pgd, p)->tlb_entry.reg.PFN = shm_pool[shmid]->pfn_start + i;
 		get_pte(mm_current->pgd, p)->tlb_entry.reg.G = 0;
 		get_pte(mm_current->pgd, p)->tlb_entry.reg.D = 1;
 		get_pte(mm_current->pgd, p)->tlb_entry.reg.V = 1;
